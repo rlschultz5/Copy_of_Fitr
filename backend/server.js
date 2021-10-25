26ed50d7f8ca013e094087302dc2997c83ce27f9
SammyG6 <sgomez6@wisc.edu>
2021-10-24 21:11:38 -0500

    added basic stuff
    added some files that we will eventually need. more importantly added the connection to the database. Have to change the database url still i just used the old one from the spike exercise to test. Tested and the server still runs and successfully connects to database

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,30 @@
 const express = require('express')
+const dbConfig = require("./config/db.config")
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+
+const db = require("./models")
+
+db.mongoose.connect(dbConfig.CONNECTION_STRING, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log("Successfully connected to db.");
+    dbInitialize();
+  })
+  .catch((err) => {
+    console.error("Error when connecting to db", err);
+    process.exit();
+  });
+
+dbInitialize = () => {
+  
+}
+
 
 app.get("/", (req, res) => {
     res.json({message: "Please give us an A :)"});
