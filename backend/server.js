fb4b8f0543a958ff5a1570cd48baf3d858e600f3
SammyG6 <sgomez6@wisc.edu>
2021-10-27 16:11:32 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,8 @@ db.mongoose.connect(dbConfig.CONNECTION_STRING, {
   });
 
 dbInitialize = async () => {
-  //WorkoutLength.estimatedDocumentCount((err, count) => {
-  //if (!err && count === 0) {
+  WorkoutLength.estimatedDocumentCount( async (err, count) => {
+  if (!err && count === 0) {
     try {
       let workoutlength = await new WorkoutLength({
         length: 30
@@ -37,57 +37,57 @@ dbInitialize = async () => {
         console.log("error", err);
     }
 
-    new WorkoutLength({
-      length: 60
-    }).save(err => {
-      if (err) {
+    try {
+      let workoutlength = await new WorkoutLength({
+        length: 60
+      })
+      await workoutlength.save();
+    }
+    catch (err) {
         console.log("error", err);
-      }
+    }
 
-      console.log("added '60' to workoutlength collection");
-    });
-
-    new WorkoutLength({
-      length: 90
-    }).save(err => {
-      if (err) {
+    try {
+      let workoutlength = await new WorkoutLength({
+        length: 90
+      })
+      await workoutlength.save();
+    }
+    catch (err) {
         console.log("error", err);
-      }
+    }
 
-      console.log("added '90' to workoutlength collection");
-    });
-
-    new WorkoutLength({
-      length: 120
-    }).save(err => {
-      if (err) {
+    try {
+      let workoutlength = await new WorkoutLength({
+        length: 120
+      })
+      await workoutlength.save();
+    }
+    catch (err) {
         console.log("error", err);
-      }
+    }
 
-      console.log("added '120' to workoutlength collection");
-    });
-
-    new WorkoutLength({
-      length: 150
-    }).save(err => {
-      if (err) {
+    try {
+      let workoutlength = await new WorkoutLength({
+        length: 150
+      })
+      await workoutlength.save();
+    }
+    catch (err) {
         console.log("error", err);
-      }
+    }
 
-      console.log("added '150' to workoutlength collection");
-    });
-
-    new WorkoutLength({
-      length: 180
-    }).save(err => {
-      if (err) {
+    try {
+      let workoutlength = await new WorkoutLength({
+        length: 180
+      })
+      await workoutlength.save();
+    }
+    catch (err) {
         console.log("error", err);
-      }
-
-      console.log("added '180' to workoutlength collection");
-    });
-  //}
-//});
+    }
+  }
+})
 }
 
 
