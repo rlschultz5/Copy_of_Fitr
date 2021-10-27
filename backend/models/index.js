0b480808c2266bef968308071a8d4d8810dd143a
SammyG6 <sgomez6@wisc.edu>
2021-10-27 03:02:51 -0500

    added user model
    everything seems good so far, need to ask question about userPreferences

--- backend/models/index.js
+++ backend/models/index.js
@@ -2,5 +2,8 @@ const mongoose = require('mongoose');
 
 const db = {};
 db.mongoose = mongoose;
+db.user = require("./user.model");
+
+db.SCHOOLYEAR = ["freshman", "sophomore", "junior", "senior", "graduate", "professor"]
 
 module.exports = db;
