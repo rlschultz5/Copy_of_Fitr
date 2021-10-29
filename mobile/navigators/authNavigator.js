07faf8611f60fac1f3f4e3b6fcccf916f1a6ba96
Cody Um <yunsuum@gmail.com>
2021-10-29 13:01:02 -0500

    fixed start up bugs

--- mobile/navigators/authNavigator.js
+++ mobile/navigators/authNavigator.js
@@ -1,16 +1,14 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 
 
 import * as React from 'react';
 import AuthScreen from '../screens/auth/authScreen';
 
-const Stack = createNativeStackNavigator();
+const Stack = createStackNavigator();
 
 
-function AuthStack() {
+function AuthNavigator() {
     return (
             <Stack.Navigator initialRouteName="TEMP" screenOptions={{headerShown: false}}>
                 <Stack.Screen name="TEMP" component={AuthScreen} />
@@ -18,4 +16,4 @@ function AuthStack() {
     );
 }
 
-export default AuthStack;
+export default AuthNavigator;
--- mobile/screens/home/homeScreen.js
+++ mobile/screens/home/homeScreen.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text></Text>
+      <Text>Home</Text>
       <StatusBar style="auto" />
     </View>
   );
