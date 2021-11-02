cc68203d1dd11028b563df9512bb9f449e5e341c
YajurG <yajurgrover24@gmail.com>
2021-11-02 18:26:25 -0500

    server running. need to test endpoints

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 app.use(passport.initialize());
-require("./config/strategies.config")(passport);
+require("./passport/strategies")(passport);
 
 const db = require("./models")
 
