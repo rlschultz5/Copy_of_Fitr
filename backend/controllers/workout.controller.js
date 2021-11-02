c5aaab2f98b4a6155d8479de1d345616bc6087d0
YajurG <yajurgrover24@gmail.com>
2021-11-02 11:22:04 -0500

    workout apis in progress

--- backend/controllers/workout.controller.js
+++ backend/controllers/workout.controller.js
@@ -42,11 +42,44 @@ exports.editWorkout = async (req, res) => {
 }
 
 exports.deleteWorkout = async (req, res) => {
-    res.send({message: "this is the delete workout method"});
+    if (!req.body.id) {
+        res.status(500).send({error: "No ID was sent"});
+    }
+    try {
+        const data = await Workout.deleteOne({_id: req.body.id})
+        console.log(data);
+        if (!data || data.deletedCount == 0) {
+            res.status(500).send({error: "Workout not successfully deleted"});
+        } else {
+            res.send({message: "Successfully deleted", data: data});
+        }
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({error: err.message})
+    }
 }
 
 exports.createWorkout = async (req, res) => {
-    res.send({message: "this is the add workout method"});
+    if (!req.body.title || !req.body.activity || !req.body.minPeople || !req.body.experienceLevel || !req.body.length) {
+        res.status(500).send({error: "Require fields missing"});
+    }
+    try {
+        const workoutObj = {
+            title: req.body.title,
+            activity: req.body.activity,
+            location: req.body.location,
+            minPeople: req.body.minPeople,
+            maxPeople: req.body.maxPeople,
+            experienceLevel: req.body.experienceLevel,
+            length: req.body.length
+        }
+        const doc = await new Workout(workoutObj);
+        await doc.save()
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).send({error: err.message})
+    }
 }
 
 exports.getActivity = async (req, res) => {
