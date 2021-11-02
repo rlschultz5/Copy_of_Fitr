4e0e46f6d9b0103ecfe5f5a3372ccc3f9711ef8d
Cody Um <yunsuum@gmail.com>
2021-11-02 09:51:31 -0500

    added little styling

--- mobile/screens/home/homeScreen.js
+++ mobile/screens/home/homeScreen.js
@@ -1,39 +1,44 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import WorkoutList from './workoutList';
-import { StyleSheet, Text, View, Pressable} from 'react-native';
+import { StyleSheet, Text, View, Pressable, Image } from 'react-native';
 import Filter from './filter';
 
 const DUMMY = [{
-    title: "4v4 Basketball at the Nick!",
-    level: "All",
-    sports: "Basketball",
-    location: "Nick court 3",
-    capacity: 8,
-    filled: 3
+  title: "4v4 Basketball at the Nick!",
+  level: "All",
+  sports: "Basketball",
+  location: "Nick court 3",
+  capacity: 8,
+  filled: 3
 }, {
-    title: "Casual Jog around Campus",
-    level: "All",
-    sports: "Running",
-    location: "College Library",
-    capacity: -1,
-    filled: 3
+  title: "Casual Jog around Campus",
+  level: "All",
+  sports: "Running",
+  location: "College Library",
+  capacity: -1,
+  filled: 3
 }]
 
 export default function HomeScreen() {
 
-    const [showFilter, setFilter] = useState(false);
+  const [showFilter, setFilter] = useState(false);
 
   return (
     <View style={styles.container}>
-      <Text>Home</Text>
-      <Filter visible={showFilter} setVisible={setFilter}/>
-      <Pressable onPress={()=>setFilter(true)}>
-          <Text>
-              Filter
-          </Text>
-        </Pressable>
-        <WorkoutList data={DUMMY}/>
+      <Text style={{ top: "10%", left: "5%", fontSize: 30 }}> Home </Text>
+      <View style={styles.listContainer}>
+        <Text style={styles.title}>Workouts Available</Text>
+
+      </View>
+
+      <Filter visible={showFilter} setVisible={setFilter} />
+      <Pressable onPress={() => setFilter(true)}>
+        <Text>
+          Filter
+        </Text>
+      </Pressable>
+      <WorkoutList data={DUMMY} />
       <StatusBar style="auto" />
     </View>
   );
@@ -43,7 +48,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  title: {
+    fontSize: 20,
+    fontWeight: "500",
+  },
+  listContainer: {
+    padding: 20,
+  }
 });
