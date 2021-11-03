8a1df0421310de1bb7f7e7b607f85912c59b22af
SammyG6 <sgomez6@wisc.edu>
2021-11-03 02:33:55 -0500

    added userworkout and admin controller
    fully implemented not yet tested

--- backend/controllers/admin.controller.js
+++ backend/controllers/admin.controller.js
@@ -3,17 +3,90 @@ const User = db.user;
 const Admin = db.admin;
 
 exports.addAdmin = async (req, res) => {
-    res.send({message: "this is the add admin method"});
+  if (!req.body.user_id){
+    throw "No user id was sent"
+  }
+
+  try {
+    let user_id = req.body.user_id;
+    let options = {new: true};
+    let update = {
+      isAdmin: True
+    }
+    let result = await User.findOneAndUpdate({_id: user_id}, update, options);
+    let admin = await new Admin({
+      user_id: user_id,
+      email: result.email
+    })
+    await admin.save();
+    console.log(result);
+    console.log(admin);
+    res.send({message: "Admin status has been updated"})
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({message: "An error has occured. Please check logs"})
+  }
 }
 
 exports.removeAdmin = async (req, res) => {
-    res.send({message: "this is the remove admin method"});
+  if (!req.body.user_id){
+    throw "No user id was sent"
+  }
+
+  try {
+    let user_id = req.body.user_id;
+    let options = {new: true};
+    let update = {
+      isAdmin: False
+    }
+    let result = await User.findOneAndUpdate({_id: user_id}, update, options);
+    let delete_admin = await Admin.deleteOne({user_id: user_id});
+    console.log(result);
+    console.log(delete_admin);
+    res.send({message: "Admin status has been updated"})
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({message: "An error has occured. Please check logs"})
+  }
 }
 
 exports.getAdmin = async (req, res) => {
-    res.send({message: "this is the get admin method"});
+  if (!req.body.admin_id){
+    res.status(500).send({message: "No admin id submitted"});
+  }
+
+  try{
+    let result = await User.findOne({_id: admin_id});
+    if (!result) {
+      res.status(500).send({message: "No admin found"});
+    }
+    console.log(result);
+    res.send({data: result})
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({message: "An error has occured. Please check logs"})
+  }
 }
 
 exports.updateEmail = async (req, res) => {
-    res.send({message: "this is the update admin email method"});
+  if (!req.body.admin_id){
+    throw "No admin id was sent"
+  }
+
+  try {
+    let admin_id = req.body.admin_id;
+    let options = {new: true};
+    let update = {
+      email: req.body.email
+    }
+    let result = await Admin.findOneAndUpdate({_id: admin_id}, update, options);
+    console.log(result);
+    res.send({message: "Email has been updated"})
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({message: "An error has occured. Please check logs"})
+  }
 }
--- backend/controllers/userWorkout.controller.js
+++ backend/controllers/userWorkout.controller.js
@@ -2,7 +2,21 @@ const db = require("../models");
 const UserWorkout = db.userWorkout;
 
 exports.getParticipants = async (req, res) => {
-    res.send({message: "this is the get workout participants method"});
+  if (!req.body.workout_id){
+    res.status(500).send({message: "No workout submitted"});
+  }
+  try{
+    let workout_id = req.body.workout_id
+    let result = await UserWorkout.findOne({workout_id: workout_id});
+    if(!result){
+      res.status(500).send({message: "No workout found"})
+    }
+    console.log(result)
+    res.send({data: result})
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({message: "An error has occured. Please check logs"})
+  }
 }
 
 exports.getUserWorkouts = async (req, res) => {
@@ -10,9 +24,35 @@ exports.getUserWorkouts = async (req, res) => {
 }
 
 exports.addUserWorkout = async (req, res) => {
-    res.send({message: "this is the add userWorkout method"});
+    try{
+      let userWorkout = await new Activity({
+        user_id: req.body.user_id,
+        workout_id: req.body.workout_id
+      })
+      await userWorkout.save();
+      console.log(userWorkout);
+      res.send({message: "UserWorkout has been successfully added"})
+    } catch (err) {
+      console.log(err)
+      res.status(500).send({message: "Error occurred. Please check logs"})
+    }
 }
 
 exports.removeUserWorkout = async (req, res) => {
-    res.send({message: "this is the remove userWorkout method"});
+  if (!req.body.userworkout_id){
+    throw "no userworkout id was given"
+  }
+
+  try{
+    let userworkout_id = req.body.userworkout_id
+    let result = await UserWorkout.deleteOne({_id: userworkout_id});
+    if (!result) {
+      res.status(500).send({message: "No userworkout found"});
+    }
+    console.log(result);
+    res.send({data: result})
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({message: "Error occurred. Please check logs"});
+  }
 }
