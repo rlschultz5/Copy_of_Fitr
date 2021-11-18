9864c6244ef8b5c7a638a8fc59c8f88477be82b6
YajurG <yajurgrover24@gmail.com>
2021-11-18 13:32:34 -0600

    sending user data on signin

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -38,7 +38,8 @@ exports.signIn = async (req, res) => {
                         const token = jwt.sign({username: user.username}, 'supersecretkey'); //temp key?
                         res.send({
                             message: info.message,
-                            token: token
+                            token: token,
+                            data: user
                         })
                     })
                 }
